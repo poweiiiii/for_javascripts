79bd91cb5b27cf059f1686c8509a57e9c0c1ed42
poweiiiii <4b0g0175@stust.edu.tw>
2024-04-01 23:25:02 +0800

    update

--- for_order/order.js
+++ for_order/order.js
@@ -23,31 +23,59 @@ function Create_Web_Title(text){
     title.style.textAlign = 'center';
     return  title;
 }
-
+var Code_name = '';
 function Generate_code_room_Button(){
     var Button = document.getElementById('generate_code_button')
+    if (Button.textContent === 'Create a code!'){
+        Generate_room_code();
+    }
+    if (Button.textContent === 'Create a room!'){
+        document.getElementById('Generate_Code').innerHTML += '<br>' + 'Create Successful!';
+        window.open('Room.html', '_blank');
+        Display_Room_Name();
+    }
+
+  Change_to_room();
+
 }
-
-function Change_to_room(){
-
-}
-
-function Generate_room_code(){
+function Restart_Button(){
+    var Code_name = document.getElementById('Generate_Code');
+    Code_name.textContent = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123466789';
-    var code = ''
+    var Code_name = ''
     var charactersLength = characters.length;
     var Length = 10;
     for (i = 0 ; i < Length ; i++){
-        code += characters.charAt(Math.floor(Math.random() * charactersLength));
+        Code_name += characters.charAt(Math.floor(Math.random() * charactersLength));
 
     }
-    document.getElementById('Generate_Code').innerHTML += code;
-    var generate_code_button = document.getElementById('generate_code_button')
-    if (generate_code_button.textContent === 'Create a code!') {
-        generate_code_button.textContent = 'Create a room!';
+    document.getElementById('Generate_Code').innerHTML += Code_name;
+
+}
+
+function Change_to_room(){
+    var Button = document.getElementById('generate_code_button');
+    Button.textContent = 'Create a room!';
+}
+
+
+
+
+function Generate_room_code(){
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123466789';
+    Code_name = '';
+    var charactersLength = characters.length;
+    var Length = 10;
+    for (i = 0 ; i < Length ; i++){
+        Code_name += characters.charAt(Math.floor(Math.random() * charactersLength));
+
     }
-    if(generate_code_button.textContent === 'Create a room!'){
-        
-    }
+    document.getElementById('Generate_Code').innerHTML += Code_name;
     restrat_button.disabled = false;
+    
+}
+
+//Room scripts
+function Display_Room_Name(){
+    document.getElementById('display_code_name').innerHTML = Code_name;
 }
