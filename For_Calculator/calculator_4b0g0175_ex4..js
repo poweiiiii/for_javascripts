6d286adecae47ef7159ca18566947bdef9102e73
poweiiiii <4b0g0175@stust.edu.tw>
2024-04-12 19:52:41 +0800

    d

--- For_Calculator/calculator_4b0g0175_ex4..js
+++ For_Calculator/calculator_4b0g0175_ex4..js
@@ -8,10 +8,22 @@ function Get_All_Buttons(){
         button[i].addEventListener("click" , buttonclick);
     }
 }
-
+//實作這次的運算 不關上一次的運算
 function buttonclick(){
     val = this.value;
+
+    
     switch(val){
+        case '=':
+            let result = eval(expression)
+            dispaly.innerHTML = result;
+            break;
+
+        case 'C':
+            dispaly.innerHTML = '0';
+            expression = '';
+            break;
+
         default:
             expression += val;
             document.getElementById('dispaly').innerHTML = expression;
