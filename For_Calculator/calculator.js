5ba0eeb0b2b4206e57423f53d985cab03456ccac
poweiiiii <4b0g0175@stust.edu.tw>
2024-04-12 15:08:38 +0800

    display number on screen

--- For_Calculator/calculator.js
+++ For_Calculator/calculator.js
@@ -1,3 +1,21 @@
-function display_numbers (num){
-    document.querySelectorAll('button').values += num
+let val;
+let expression = '';
+
+function Get_All_Buttons(){
+    let button = document.querySelectorAll('button')
+
+    for (i = 0 ; i < button.length ; i++){
+        button[i].addEventListener("click" , buttonclick);
+    }
+}
+
+function buttonclick(){
+    val = this.value;
+    switch(val){
+        default:
+            expression += val;
+            document.getElementById('dispaly').innerHTML = expression;
+            break;
+    }
+
 }
