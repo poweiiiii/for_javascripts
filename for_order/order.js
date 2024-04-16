2ec257e6aea38e42e905708df658003c302255fa
poweiiiii <4b0g0175@stust.edu.tw>
2024-04-16 12:35:48 +0800

    LEARNING SOCKET

--- for_order/order.js
+++ for_order/order.js
@@ -1,4 +1,29 @@
+//Websocket
 
+//使用 WebSocket 的網址向 Server 開啟連結
+let ws = new WebSocket('ws://localhost:5500')
+
+
+
+
+//開啟後執行的動作，指定一個 function 會在連結 WebSocket 後執行
+ws.onopen = () => {
+    console.log('open connection')
+}
+//接收 Server 發送的訊息
+ws.onmessage = event => {
+    console.log(event)
+    
+}
+
+//關閉後執行的動作，指定一個 function 會在連結中斷後執行
+ws.onclose = () => {
+    console.log('close connection')
+}
+
+//接收 Server 發送的訊息
+
+//
 function Display_code_name(){
     Push_room_to_array ();
     var code_name = document.getElementById('code_name_input').value;
